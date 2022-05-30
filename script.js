6e717a055976d56f98cee1b471a5768dfa44b984
Abdullah Alzahrani <abaade-1@hotmail.com>
2022-05-30 20:46:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("page loaded...");
+var x = document.getElementsByClassName("placeholder")
+
+function play(element){
+    element.play()
+}
+function stop(element){
+    element.pause()
+}
